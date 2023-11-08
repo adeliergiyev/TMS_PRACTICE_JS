6373bb75950e63bf2d58763ff74105ffdb17d757
Oleksandr Deliergiyev <adeliergiev@gmail.com>
2023-11-08 08:52:16 +0100

    build(hw_3): Tasks Homework

--- Lesson_3/Theory_L3/src/js/theory_l3_basic_operators.js
+++ Lesson_3/Theory_L3/src/js/theory_l3_basic_operators.js
@@ -1,4 +1,6 @@
-//БAЗОВЫЕ ОПЕРАТОРЫ
+//Статьи из https://learn.javascript.ru/ 
+
+//ОПЕРАТОРЫ
 
 //1.Присваивание
 //2. Математические
@@ -9,10 +11,15 @@
 //7. Логические
 
 //Преобразование типов:
+
+let t = 0;
+
 //Task 1. 
 // Какой результат будет у выражений ниже?
 console.log("");
 console.log("Task 1:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
 let a1 = "" + 1 + 0, // = 10;
     a2 = "" - 1 + 0, // = -1;
     a3 = true + false, // = 1; 
@@ -57,6 +64,7 @@ console.log(a14);
 // Каким будет результат этих выражений?
 console.log("");
 console.log("Task 2:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
 
 let b1 = 5 > 4, // = true;
     b2 = "ананас" > "яблоко", // = false;
@@ -89,6 +97,7 @@ console.log(b7);
 // Получение первого истинного значения из списка переменных или выражений.
 console.log("");
 console.log("Task 3:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
 
 let firstNameTask3 = prompt("First Name:"),
     lastNameTask3 = prompt("Last Name:"),
@@ -100,6 +109,7 @@ console.log(firstNameTask3 || lastNameTask3 || nicktNameTask3 || "Please enter y
 // Мы уже знаем, что ИЛИ || возвращает первое истинное значение, поэтому, если a является таковым, вычисление до правой части выражения не дойдёт.
 console.log("");
 console.log("Task 4:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
 
 let yorCar = prompt("Enter brand of your car:");
 
@@ -110,6 +120,7 @@ console.log(yorCar);
 // Напишите условие if для проверки, что переменная age находится в диапазоне между 14 и 90 включительно. «Включительно» означает, что значение переменной age может быть равно 14 или 90.
 console.log("");
 console.log("Task 5:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
 
 let ageTask5 = +prompt("Enter your age:");
 
@@ -129,6 +140,7 @@ else {
 
 console.log("");
 console.log("Task 6:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
 
 let userNameTask6 = prompt("Enter please your Username:"), //Ввыводим промт для ввода имя пользователя;
     userCanceledTask6 = "Cancled!", // Объявляем переменные и присваиваем значения с сообщениями для пользлвателя; 
@@ -144,7 +156,7 @@ if (userNameTask6 === "Admin") { // Задаём условия, что если
     } else if (userPassTask6 === null) { // Условие: если пользователь нажал Esc тогда выводим сообщение об отмене; 
         alert(userCanceledTask6); // Вывод сообщения об отмене; 
     }
-    else{
+    else {
         alert(incorrectPassTask6); // В противном случае выводим сообщение об некорректном пароле; 
     }
 }
@@ -157,5 +169,222 @@ else {
     }
 }
 
+//Видео урок:
+//Task 7. 
+// Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. Если это так - выведите 'да', в противном случае выведите 'нет'
+console.log("");
+console.log("Task 7:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
 
+let strTask7 = "123321";
+let strFirstPartTask7 = +(strTask7[0]) + +(strTask7[1]) + +(strTask7[2]);
+let strSecondPartTask7 = +(strTask7[3]) + +(strTask7[4]) + +(strTask7[5]);
+
+//----Long variant---------//
+// if (strFirstPartTask7 === strSecondPartTask7){
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+//-----Short Variant-------//
+(strFirstPartTask7 === strSecondPartTask7) ? console.log("Yes") : console.log("No");
+
+// let i = 0;
+// while (i++ < 5) alert( i ); 
+
+
+//Task 8. 
+// Выведите числа от 1 до 50 и от 35 до 8
+console.log("");
+console.log("Task 8:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+let i = 1;
+
+while (i <= 50) {
+    document.write(i + "<br>");
+    i++;
+}
+
+let j = 35;
+while (j >= 8) {
+    document.write(j + "<br>");
+    j--;
+}
+
+//Task 9. 
+//Выведите столбец чисел от 89 до 11 - воспользуйтесь циклом while и отделите числа // тегом <br /> друг от друга, чтобы получить столбец, а не строку.
+console.log("");
+console.log("Task 9:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+
+for (let a = 89; a >= 11; a--){
+    document.write("<div class = 'script__redColumn'>" + a + "</div><br>");
+    console.log(a);
+}
+
+//Task 10. 
+//С помощью цикла найдите сумму чисел от 0 до 100.
+console.log("");
+console.log("Task 10:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+let sum = 0;
+
+for (let aTask10 = 0; aTask10 <= 100; aTask10++){
+    document.write("<div class = 'script__redColumn'>" + aTask10 + "</div><br>");
+    sum = sum + aTask10;
+}
+document.write("<div class = 'script__redColumn_summ'>" + sum + "</div><br>");
+
+//Task 11. 
+//Дано число п=1000. Делите его на 2 столько раз, пока результат деления не станет меньше 50. Какое число получится? Посчитайте количество итераций, необходимых И для этого (итерация - это проход цикла), и запишите его в переменную num. 
+console.log("");
+console.log("Task 11:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+let numTask11 = 0;
+    nTask11 = 1000; 
+
+//     for (let nTask11 = 1000; nTask11 >= 50; nTask11 /= 2){
+//          numTask11 = ++numTask11;
+//          document.write("<div class = 'script__redColumn-iteration'>" + nTask11 + "</div><br>");
+// }
+// document.write("<div class = 'script__redColumn-numbers'>Number of iteration: " + numTask11 + "</div><br>");
+
+while (nTask11 >= 50) {
+    nTask11 = nTask11 /2;
+    numTask11++;
+    document.write("<div class = 'script__redColumn-iteration'>" + nTask11 + "</div><br>");
+    
+}
+document.write("<div class = 'script__redColumn-numbers'>Number of iteration: " + numTask11 + "</div><br>");
+
+//Task 12. 
+//Вывести таблицу умножения
+console.log("");
+console.log("Task 12:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+for (let iTask12 = 1; iTask12 <= 10; iTask12++){
+    for(let jTask12 = 1; jTask12 <= 10; jTask12++){ //Внутренний цикл работает так что должны пройти все итерации внутреннего цикла и только потом запускается следуюзая итерация первого цикла;
+        document.write(`${iTask12} * ${jTask12} = ${iTask12 * jTask12} <br>`);
+    }
+    document.write(`<div class = "script__table-task12"></div>`);
+}
+
+//Task 13. 
+//Вывести период год на основании номера месяца. Через switch
+console.log("");
+console.log("Task 13:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+
+// Solution thrue the operator SWITCH:
+let xTask13 = +prompt("Enter please your number from 1 to 12: ", 4); // Через prompt задаём значение переменной;
+
+switch(xTask13) { //Конструкция "switch" (используется для условий);
+    case 3 : // если значение xTask13 = 3, тогда в консоле выведеться текст "It's Spring";
+    case 4 : // если значение xTask13 = 4, тогда в консоле выведеться текст "It's Spring";
+    case 5 : // если значение xTask13 = 5, тогда в консоле выведеться текст "It's Spring";
+        console.log("It's Spring");
+        break; // Останавлиет сравнение; 
+    case 6 : // если значение xTask13 = 6, тогда в консоле выведеться текст "It's Summer";
+    case 7 : // если значение xTask13 = 7, тогда в консоле выведеться текст "It's Summer";
+    case 8 : // если значение xTask13 = 8, тогда в консоле выведеться текст "It's Summer";
+        console.log("It's Summer");
+        break; // Останавлиет сравнение; 
+    case 9 : // если значение xTask13 = 9, тогда в консоле выведеться текст "It's Automn";
+    case 10 : // если значение xTask13 = 10, тогда в консоле выведеться текст "It's Automn";
+    case 11 : // если значение xTask13 = 11, тогда в консоле выведеться текст "It's Automn";
+        console.log("It's Automn");
+        break; // Останавлиет сравнение; 
+    case 12 : // если значение xTask13 = 12, тогда в консоле выведеться текст "It's Winter";
+    case 1 : // если значение xTask13 = 1, тогда в консоле выведеться текст "It's Winter";
+    case 2 : // если значение xTask13 = 2, тогда в консоле выведеться текст "It's Winter";
+        console.log("It's Winter");
+        break; // Останавлиет сравнение; 
+    default:
+        console.log("This number is isncorrect.");  // если значение xTask13 < 1 или > 12 или не числовое значени, тогда в консоле выведеться текст "It's Winter";
+        break;
+}
+
+// Solution thrue the operator SWITCH + if...else, для подусловия:
+let yTask13 = +prompt("Enter please your number from 1 to 12: ", 4); // Через prompt задаём значение переменной;
+
+switch(yTask13) {
+    case 3 : // если значение xTask13 = 3, тогда в консоле выведеться текст "It's Spring";
+    case 4 : // если значение xTask13 = 4, тогда в консоле выведеться текст "It's Spring";
+    case 5 : // если значение xTask13 = 5, тогда в консоле выведеться текст "It's Spring";
+        console.log("It's Spring");
+        if(yTask13 == 3){console.log("This month is Mart")}
+        else if(yTask13 == 4){console.log("This month is April")}
+        else{console.log("This month is May")}
+        break; // Останавлиет сравнение; 
+    case 6 : // если значение xTask13 = 6, тогда в консоле выведеться текст "It's Summer";
+    case 7 : // если значение xTask13 = 7, тогда в консоле выведеться текст "It's Summer";
+    case 8 : // если значение xTask13 = 8, тогда в консоле выведеться текст "It's Summer";
+        console.log("It's Summer");
+        if(yTask13 == 6){console.log("This month is June")}
+        else if(yTask13 == 7){console.log("This month is July")}
+        else{console.log("This month is August")}
+        break; // Останавлиет сравнение; 
+    case 9 : // если значение xTask13 = 9, тогда в консоле выведеться текст "It's Automn";
+    case 10 : // если значение xTask13 = 10, тогда в консоле выведеться текст "It's Automn";
+    case 11 : // если значение xTask13 = 11, тогда в консоле выведеться текст "It's Automn";
+        console.log("It's Automn");
+        if(yTask13 == 9){console.log("This month is Septemver")}
+        else if(yTask13 == 10){console.log("This month is October")}
+        else{console.log("This month is November")}
+        break; // Останавлиет сравнение; 
+    case 12 : // если значение xTask13 = 12, тогда в консоле выведеться текст "It's Winter";
+    case 1 : // если значение xTask13 = 1, тогда в консоле выведеться текст "It's Winter";
+    case 2 : // если значение xTask13 = 2, тогда в консоле выведеться текст "It's Winter";
+        console.log("It's Winter");
+        if(yTask13 == 12){console.log("This month is December")}
+        else if(yTask13 == 1){console.log("This month is January")}
+        else{console.log("This month is Febrery")}
+        break; // Останавлиет сравнение; 
+    default:
+        console.log("This number is isncorrect.");  // если значение xTask13 < 1 или > 12 или не числовое значени, тогда в консоле выведеться текст "It's Winter";
+        break;
+}
+
+
+//Task 14. 
+//Напишите цикл, который предлагает prompt ввести число, большее 100. Если посетитель ввёл другое число – попросить ввести ещё раз, и так далее.
+//Цикл должен спрашивать число пока либо посетитель не введёт число, большее 100, либо не нажмёт кнопку Отмена (ESC).
+//Предполагается, что посетитель вводит только числа. Предусматривать обработку нечисловых строк в этой задаче необязательно.
+console.log("");
+console.log("Task 14:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+// let numberTask14;
+
+// do {
+//     numberTask14 = +prompt("Enter any number more than 100: ", 0);
+//     console.log(numberTask14);
+// } while (numberTask14 <= 100 && numberTask14);
+
+//Task 15. 
+//Натуральное число, большее 1, называется простым, если оно ни на что не делится, кроме себя и 1.
+//Другими словами, n > 1 – простое, если при его делении на любое число кроме 1 и n есть остаток.
+//Например, 5 это простое число, оно не может быть разделено без остатка на 2, 3 и 4.
+//Напишите код, который выводит все простые числа из интервала от 2 до n.
+//Для n = 10 результат должен быть 2,3,5,7.
+//P.S. Код также должен легко модифицироваться для любых других интервалов.
+
+console.log("");
+console.log("Task 15:");
+document.write("<h2 class = 'script__redColumn-title'>Task" + " " + ++t + "</h2><br>");
+
+// let nTask15 = 10, 
+//     kTask15 = 2,
+//     tTask15; 
+
+//     while (kTask15 <= 10){
+//     if (nTask15 % kTask15++ !== 0) console.log(kTask15);
+// }
 
