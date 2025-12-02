413b588e42e3be854a4dedd83fc4846a2a12cfa5
Oleksandr Deliergiyev <adeliergiev@gmail.com>
2025-12-02 17:00:22 +0100

    home-work-6-7-objects

---
+++ Lesson_6-7-Objects/Class-Work/src/js/classWork6-7.js
@@ -0,0 +1,25 @@
+//Example 1: 
+//debugger;
+globalVariable = 1000;
+
+let ternarnyOperator = globalVariable ? globalVariable : 200;
+
+console.log(ternarnyOperator);
+
+document.querySelector('.contentResult').innerHTML = ternarnyOperator;
+
+//Example 2: Objects
+
+const iphone = {
+    color: "black",
+    width: 20, 
+    height: 40,
+    weght: 800,
+    camera: {
+        count: 3,
+        model: "t100-samsung",
+        countPixels: 1000
+    }
+}
+
+console.table(iphone);
