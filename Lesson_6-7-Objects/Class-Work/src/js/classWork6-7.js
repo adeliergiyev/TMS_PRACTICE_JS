b01f30cc5233feba7ded40620cc493a70da14a08
oleksandr.deliergiye <oleksandr.deliergiyev@nuvia.cz>
2025-12-03 15:11:31 +0100

    Added new Odjects-2

--- Lesson_6-7-Objects/Class-Work/src/js/classWork6-7.js
+++ Lesson_6-7-Objects/Class-Work/src/js/classWork6-7.js
@@ -56,3 +56,48 @@ const iphoneObjectConst = { ...iphone }; //new constant object
 
 console.table(iphoneObjectConst.color); // black
 
+const iphoneProNew = { ...iphonePro,
+                        camera: {
+                            ...iphonePro.model,
+                        }
+};
+
+console.log("iphoneProNew: " + iphoneProNew);
+
+        //Example 4. Copying objects with nested objects:
+        const iphoneProNew = { ...iphonePro,
+                                 camera: {
+                                 ...iphonePro.camera,
+            }
+        };
+
+        iphonePro.camera.model = "t200-apple";
+
+        console.log("iphonePro: " + iphonePro.camera.model);
+        console.log("iphoneProNew: " + iphoneProNew.camera.model);
+
+//Tasks.
+// 1. Проверьте, что этот объект не пустой и что в нем есть ключ age.
+        const user = {
+            location: "London",
+            age: 30,
+            name: "John"
+        };
+
+        if (Object.keys(user).length === 0) {
+            // Checking empty:
+            console.log("Object is empty");
+        } else {
+            // Checking if object conains key = "John":
+            for (let key in user) {
+                const checkObjectKey = user[key];
+                if (checkObjectKey === 'John') {
+                    console.log("John Key Is: " + true);
+                } else {
+                    console.log("John Key Is: " + false);
+                }
+            }
+        }
+
+
+
